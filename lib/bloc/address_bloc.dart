3b86d4016a5b382359305df3e1ecbc763493dccb
Tegar <artphonegraphy@gmail.com>
2020-01-28 23:32:40 +0700

    Add internet access permission

--- lib/bloc/address_bloc.dart
+++ lib/bloc/address_bloc.dart
@@ -9,7 +9,9 @@ class AddressBloc extends Bloc<Address, String> {
 
   @override
   Stream<String> mapEventToState(Address event) async* {
-    _address = (event == Address.Rumah) ? "RUMAH" : "KANTOR";
+    _address = (event == Address.Rumah)
+        ? "RUMAH"
+        : (event == Address.Kantor) ? "KANTOR" : "Tidak ada";
     yield _address;
   }
 }
